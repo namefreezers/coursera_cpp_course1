4f38bb3d07019c241fccabc40d3443a39779e909
Name <freezers@ukr.net>
2019-10-29 23:13:29 +0200

    week 5 final proj draft 1

--- week5/w5_final_project/src/w5_final_project.cpp
+++ week5/w5_final_project/src/w5_final_project.cpp
@@ -12,36 +12,112 @@ using namespace std;
 
 class Date {
 public:
-  int GetYear() const;
-  int GetMonth() const;
-  int GetDay() const;
+	int GetYear() const;
+	int GetMonth() const;
+	int GetDay() const;
+
+	void SetDate(int new_year, int new_month, int new_day) {
+		if (new_month < 1 || new_month > 12) {
+			throw invalid_argument(
+					"Month value is invalid: " + to_string(new_month));
+		}
+		if (new_day < 1 || new_day > 31) {
+			throw invalid_argument(
+					"Day value is invalid: " + to_string(new_day));
+		}
+		year = new_year;
+		month = new_month;
+		day = new_day;
+	}
+
+private:
+	int year;
+	int month;
+	int day;
 };
 
-istream& operator>>(istream)
+istream& operator>>(istream &stream, Date &d) {
+	string date_str;
+	stream >> date_str;
 
-bool operator<(const Date& lhs, const Date& rhs);
+	stringstream ss(date_str);
+	int year, month, day;
+	if (!(ss >> year)) {
+		throw runtime_error("Wrong date format: " + date_str);
+	}
+	if (ss.peek() != '-') {
+		throw runtime_error("Wrong date format: " + date_str);
+	}
+	ss.ignore(1);
+	if (!(ss >> month)) {
+		throw runtime_error("Wrong date format: " + date_str);
+	}
+	if (ss.peek() != '-') {
+		throw runtime_error("Wrong date format: " + date_str);
+	}
+	ss.ignore(1);
+	if (!(ss >> day)) {
+		throw runtime_error("Wrong date format: " + date_str);
+	}
+	string temp;
+	if (ss >> temp) { // anything left
+		throw runtime_error("Wrong date format: " + date_str);
+	}
+
+	d.SetDate(year, month, day);
+	return stream;
+}
+
+bool operator<(const Date &lhs, const Date &rhs);
 
 class Database {
 public:
-  void AddEvent(const Date& date, const string& event);
-  bool DeleteEvent(const Date& date, const string& event);
-  int  DeleteDate(const Date& date);
+	void AddEvent(const Date &date, const string &event);
+	bool DeleteEvent(const Date &date, const string &event);
+	int DeleteDate(const Date &date);
 
-  set<string> Find(const Date& date) const;
+	set<string> Find(const Date &date) const;
 
-  void Print() const;
+	void Print() const;
 };
 
 int main() {
-  Database db;
+	Database db;
 
-  string command;
-  while (getline(cin, command)) {
-    stringstream ss(command);
-    if ()
+	string command;
+	while (getline(cin, command)) {
+		stringstream ss_line(command);
 
+		string op;
+		if (!(ss_line >> op)) {
+			continue;
+		}
 
-  }
+		Date d;
+		if (op == "Add" or op == "Del" or op == "Find") {
+			try {
+				ss_line >> d;
+			} catch (const invalid_argument &e) {
+				cout << e.what() << endl;
+				continue;
+			} catch (const runtime_error &e) {
+				cout << e.what() << endl;
+				continue;
+			}
+		}
 
-  return 0;
-}
+		string event;
+		bool event_present;
+		event_present = (ss_line >> event);
+		if (op == "Add") {
+			db.AddEvent(d, event);
+		} else if (op == "Del" && event_present) {
+			db.DeleteEvent(d, event);
+		} else if (op == "Del") {
+			db.DeleteDate(d);
+		} else if (op == "Find") {
+			for ()
+		}
+
+		return 0;
+	}
